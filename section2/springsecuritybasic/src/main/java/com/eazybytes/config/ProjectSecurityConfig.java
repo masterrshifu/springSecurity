e1cff44fb3df25d725c9d7e85f856c0cc82edc03
masterrshifu <programmingwithakshay@gmail.com>
2023-04-29 13:38:12 +0530

    first commit

---
+++ section2/springsecuritybasic/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -0,0 +1,32 @@
+package com.eazybytes.config;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class ProjectSecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+
+        /*
+        * Below is the custom security configurations
+        * */
+
+        http.authorizeHttpRequests()
+                        .requestMatchers("/myAccount","/myBalance","/myLoans","/myCards").authenticated()
+                        .requestMatchers("/contact","/notices").permitAll()
+                        .and().formLogin()
+                        .and().httpBasic();
+        return http.build();
+
+//        http.authorizeHttpRequests().anyRequest().permitAll()
+//                .and().formLogin()
+//                .and().httpBasic();
+//        return http.build();
+    }
+}
